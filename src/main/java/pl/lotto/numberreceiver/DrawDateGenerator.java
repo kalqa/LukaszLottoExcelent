5df8bad0eb3a89a428f9bdb96e66c1f1e68f7517
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2022-10-25 10:52:03 +0200

    first_review

--- src/main/java/pl/lotto/numberreceiver/DrawDateGenerator.java
+++ src/main/java/pl/lotto/numberreceiver/DrawDateGenerator.java
@@ -1,11 +1,25 @@
 package pl.lotto.numberreceiver;
 
+import java.time.Clock;
+import java.time.DayOfWeek;
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
 
 import static pl.lotto.numberreceiver.DateMessageProvider.*;
 
 public class DrawDateGenerator {
+
+    private final Clock clock;
+
+    DrawDateGenerator(Clock clock) {
+        this.clock = clock;
+    }
+
     LocalDateTime generateDrawDate() {
-        return LocalDateTime.of(YEAR, MONTH, DAY, HOUR, MINUTES);
+        LocalDateTime now = LocalDateTime.now(clock);
+        TemporalAdjusters.next(DayOfWeek.SATURDAY);
+        return now;
+//        return LocalDateTime.of(YEAR, MONTH, DAY, HOUR, MINUTES);
     }
 }
