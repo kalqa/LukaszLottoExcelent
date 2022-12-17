9dfc5ea5f6e7e39945d46840866b5c84a946083c
Łukasz Nowogórski <luxus0@gmail.com>
2022-12-17 15:59:59 +0100

    Add method getResults(LocalDateTime)

--- src/main/java/pl/lotto/resultannouncer/ResultAnnouncerFacade.java
+++ src/main/java/pl/lotto/resultannouncer/ResultAnnouncerFacade.java
@@ -2,8 +2,10 @@ package pl.lotto.resultannouncer;
 
 import pl.lotto.resultannouncer.dto.ResultAnnouncerDto;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import static java.util.Objects.requireNonNull;
 import static pl.lotto.resultannouncer.ResultAnnouncerMessage.WIN;
 
 public class ResultAnnouncerFacade {
@@ -16,8 +18,17 @@ public class ResultAnnouncerFacade {
     }
 
     public ResultAnnouncerDto getResults(UUID uuid) {
-        if (uuid != null) {
-            ResultAnnouncer resultAnnouncer = resultAnnouncerRepository.findResultLottoByDate(dateTimeDrawResult.generateNextDrawDate());
+            requireNonNull(uuid);
+            ResultAnnouncer resultAnnouncer = resultAnnouncerRepository.findResultLottoByUUID(uuid);
+            ResultAnnouncer createdResultAnnouncer = new ResultAnnouncer(resultAnnouncer.uuid, resultAnnouncer.inputNumbers, resultAnnouncer.drawDateTime);
+            ResultAnnouncer savedResultAnnouncer = resultAnnouncerRepository.save(createdResultAnnouncer);
+            return new ResultAnnouncerDto(savedResultAnnouncer.uuid, savedResultAnnouncer.inputNumbers, savedResultAnnouncer.drawDateTime, WIN);
+    }
+
+    public ResultAnnouncerDto getResults(LocalDateTime dateTimeLotto) {
+        LocalDateTime nexDateTimeLotto = dateTimeDrawResult.generateNextDrawDate();
+        if (dateTimeLotto != null && dateTimeLotto.equals(nexDateTimeLotto)) {
+            ResultAnnouncer resultAnnouncer = resultAnnouncerRepository.findResultLottoByDate(dateTimeLotto);
             ResultAnnouncer createdResultAnnouncer = new ResultAnnouncer(resultAnnouncer.uuid, resultAnnouncer.inputNumbers, resultAnnouncer.drawDateTime);
             ResultAnnouncer savedResultAnnouncer = resultAnnouncerRepository.save(createdResultAnnouncer);
             return new ResultAnnouncerDto(savedResultAnnouncer.uuid, savedResultAnnouncer.inputNumbers, savedResultAnnouncer.drawDateTime, WIN);
